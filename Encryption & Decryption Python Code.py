87340e26f6067a904be4ea0bf2de9c01fc318062
evenkeon <salunkhesamay@gmail.com>
2025-02-24 18:32:06 +0530

    Update Encryption & Decryption Python Code.py

--- Encryption & Decryption Python Code.py
+++ Encryption & Decryption Python Code.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import string
 
-img = cv2.imread("Iron Man.jpg") # Replace with the correct image path
+img = cv2.imread("TonyStark.jpg") # Replace with the correct image path
 
 msg = input("Enter secret message:")
 password = input("Enter a passcode:")
